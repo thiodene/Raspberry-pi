6e113c3f3059664d263590e79895b70d60fb07ab
Serge Ayissi <ayissi.serge@gmail.com>
2018-09-06 09:25:37 -0400

    Create error_msg_startup.sh

---
+++ startup/error_msg_startup.sh
@@ -0,0 +1,7 @@
+# Startup message error on the Raspberry Pi
+# Source: https://lb.raspberrypi.org/forums/viewtopic.php?t=171843
+# I found this on an Ubuntu forum; it worked for me
+
+sudo apt-get install xserver-xorg-legacy
+
+# so I assume that means it is an issue with the xserver upgrade.
