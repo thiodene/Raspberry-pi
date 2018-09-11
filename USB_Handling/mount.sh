1acce301facdce8450a201d92b81d363b2dd0299
Serge Ayissi <ayissi.serge@gmail.com>
2018-09-11 15:34:14 -0400

    Update mount.sh

--- USB_Handling/mount.sh
+++ USB_Handling/mount.sh
@@ -1,2 +1,8 @@
 # Mount the USB key port to mnt (could be different than sda1... could be sdb/ sda/ etc...)
 sudo mount /dev/sda1 /mnt
+
+# Use the data
+cd /mnt
+
+#unmount when finished using it
+umount /mnt
